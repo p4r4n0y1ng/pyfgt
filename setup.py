1304488791500ef3f662b178f4c8e48d40152960
James Huber <huberj001@gmail.com>
2018-09-13 10:43:33 -0400

    modified setup.py only to get README.md to pypi.org

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_desc = fh.read()
+
 setup(
     name='pyfgt',
-    version='0.5.0',
+    version='0.5.1',
     packages=find_packages(),
     url='https://github.com/p4r4n0y1ng/pyfgt',
     license='Apache 2.0',
     author='p4r4n0y1ng',
     author_email='jhuber@fortinet.com',
     description='Represents the base components of the Fortinet FortiGate REST interface with abstractions',
+    long_description=long_desc,
+    long_description_content_type="text/markdown",
     include_package_data=True,
     install_requires=['requests']
 )
